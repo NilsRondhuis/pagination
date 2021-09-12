13bcfc1dbc8135f332b1700bf20730ac8e7b4914
NilsRondhuis <nilsrondhuis@ukr.net>
2021-09-12 12:02:56 +0300

    Готовая пагинация, возможно переписать на классы кнопку

--- src/js/main.js
+++ src/js/main.js
@@ -8,25 +8,66 @@ const refs = {
     input: document.querySelector('.input'),
     list: document.querySelector('.list'),
     loadBtn: document.querySelector('.btn-load'),
+    spinner: document.querySelector('.spinner'),
+    textcontentBtnLoad: document.querySelector('.textcontent'),
 };
 
 refs.form.addEventListener('submit', onSearch);
-refs.loadBtn.addEventListener('click', onLoad);
+refs.loadBtn.addEventListener('click', onLoadButton);
 
 
 function onSearch(e) {
     e.preventDefault();
     newsApiService.query = e.currentTarget.elements.query.value;
+    if (newsApiService.query === '') {
+        return;
+    }
     newsApiService.resetPage();
     // ресет делается при сабмите формы
     // на методе класса збрасывает на 1
 
-    newsApiService.fetchArticles().then(appendArticlesMarkup);
+    clearArticlesContainer()
+    // при сабмите формы делается очистка
+    // контейнера, когда вводишь запрос на
+    // новую новость
+
+    refs.loadBtn.classList.remove('is-hidden');
+    refs.textcontentBtnLoad.textContent = 'Загружаю';
+    refs.spinner.classList.remove('is-hidden');
+    // при сабмите формы появляется кнопка
+    // и появляется спиннер загрузки
+    // так же при сабмите появляется текст
+    // что загружает
+
+    newsApiService.fetchArticles().then(article => {
+        appendArticlesMarkup(article);
+        refs.spinner.classList.add('is-hidden');
+        // при успешном запроссе скрывается спиннер
+
+        refs.textcontentBtnLoad.textContent = 'Загрузить еще';
+        // при успешном пароссе текст меняется
+    });
     e.currentTarget.reset();
 }
 
-function onLoad() {
-    newsApiService.fetchArticles();
+refs.loadBtn.classList.add('is-hidden')
+// кнопка скрыта изначально
+
+function onLoadButton() {
+    refs.spinner.classList.remove('is-hidden');
+    // при клике загрузить еще появляется спиннер
+
+    refs.textcontentBtnLoad.textContent = 'Загружаю';
+    // при клике на кнопку появляется такой текст
+
+    newsApiService.fetchArticles().then(article => {
+        appendArticlesMarkup(article);
+        refs.spinner.classList.add('is-hidden');
+        // при успешном запроссе скрывается спиннер
+
+        // при успешном запроссе меняется текст
+        refs.textcontentBtnLoad.textContent = 'Загрузить еще';
+    });
 }
 
 function appendArticlesMarkup(articles) {
---
+++ src/js/partials/load-btn.js
@@ -0,0 +1,7 @@
+class LoadBtn {
+    constructor() {
+
+    }
+
+    
+}
